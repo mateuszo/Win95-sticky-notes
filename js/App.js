5f65efa39d6c420a087fe3706f4769068c3ce4a0
Mateusz Ostafil <mateusz.ostafil@codecool.com>
2018-03-30 11:21:08 +0200

    Remove unused code

--- js/App.js
+++ js/App.js
@@ -1,17 +1,7 @@
 "use strict";
 
-import { Note } from "./Note/Note.js";
-import { NoteController } from "./Note/NoteController.js";
-import { NoteView } from "./Note/NoteView.js";
 import {NewButtonController} from "./NewNoteButton/NewButtonController.js";
 import {NewButtonView} from "./NewNoteButton/NewButtonView.js";
-
-// var note = new Note();
-// note.title = "hello";
-// note.content = "world";
-//
-// var noteController = new NoteController(note);
-// var noteView = new NoteView(noteController);
 
 
 let buttonController = new NewButtonController();
