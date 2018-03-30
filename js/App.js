0b6e4c6cfd1b9ba92d9fcad7e02b535fd34bc340
Mateusz Ostafil <mateusz.ostafil@codecool.com>
2018-03-30 11:15:18 +0200

    Move note specific files to separate directory

--- js/App.js
+++ js/App.js
@@ -1,18 +1,15 @@
 "use strict";
 
-import { Note } from "./Note.js";
-import { NoteController } from "./NoteController.js";
-import { NoteView } from "./NoteView.js";
+import { Note } from "./Note/Note.js";
+import { NoteController } from "./Note/NoteController.js";
+import { NoteView } from "./Note/NoteView.js";
 
 var note = new Note();
 note.title = "hello";
 note.content = "world";
 
-
 var noteController = new NoteController(note);
-
 var noteView = new NoteView(noteController);
 
 let container = document.getElementsByClassName("container")[0];
-
 container.appendChild(noteView.element);
